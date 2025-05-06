b79934f9987785ff3ddfdb25d24025d4bf93984c
Kroniichiwa <160051220+DanJC113@users.noreply.github.com>
2025-05-06 17:54:45 -0600

    Semana 4

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
+
+[RequireComponent (typeof(Animator))]
 
 public class Bomba : MonoBehaviour
 {
+    [SerializeField] int gridOffset;
+    [SerializeField] int spawnHeight;
+
     [Header("Explotion Cast")]
     [SerializeField] float sphereCastRad;
     [SerializeField] LayerMask layerMask;
@@ -11,27 +17,57 @@ public class Bomba : MonoBehaviour
     [SerializeField] float explotionTimer;
     float spawnTime;
 
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnEnable()
     {
         spawnTime = Time.time;
+        Vector2 spawnPos = new Vector2(transform.position.x, transform.position.z);
+        // X
+        int divMain = (int)Mathf.Floor(spawnPos.x / gridOffset);
+        float module = spawnPos.x % gridOffset;
+        if (Math.Abs(module) > gridOffset / 2) divMain++;
+        spawnPos.x = divMain * gridOffset;
+        // Y (Z)
+        divMain = (int)Mathf.Floor(Math.Abs(spawnPos.y / gridOffset));
+        module = spawnPos.y % gridOffset;
+        if (Math.Abs(module) > gridOffset / 2) divMain++;
+        spawnPos.y = divMain * -gridOffset;
+
+        transform.position = new Vector3(spawnPos.x, spawnHeight, spawnPos.y);
     }
 
     private void Update()
     {
         if(Time.time -  spawnTime >= explotionTimer)
         {
-            Explode();
-            //gameObject.SetActive(false);
+            animator.SetTrigger("Explode");
+            spawnTime = Time.time;
         }
     }
 
-    void Explode()
+    public void Explode()
     {
-        Ray ray = new Ray(transform.position, Vector3.right);
+        ExplodeInDirection(Vector3.right);
+        ExplodeInDirection(Vector3.left);
+        ExplodeInDirection(Vector3.forward);
+        ExplodeInDirection(Vector3.back);
+    }
+
+    void ExplodeInDirection(Vector3 direction)
+    {
+        Ray ray = new Ray(transform.position, direction);
 
         RaycastHit[] hits = Physics.SphereCastAll(ray, sphereCastRad, range, layerMask);
 
-        if (hits.Length > 0 )
+        Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+
+        if (hits.Length > 0)
         {
             foreach (RaycastHit hit in hits)
             {
@@ -40,5 +76,10 @@ public class Bomba : MonoBehaviour
                 if (hit.transform.tag == "Breakable") break;
             }
         }
+    }
+
+    public void DisableObject()
+    {
+        gameObject.SetActive(false);
     }
 }
